96811daa432b819500b05cc784d78800136a1450
Carl-Robert Mõts <carl-robert.mots@khk.ee>
2021-10-28 10:47:18 +0300

    if, else

---
+++ jaatumine21.js
@@ -0,0 +1,8 @@
+//kasutaja sisend
+let temperatuur = prompt( "Sisesta temperatuur")
+
+if(temperatuur > 4.0) {
+    console.log("Ei ole jäätumise ohtu")
+} else {
+    console.log("On jäätumise ohtu")
+}
